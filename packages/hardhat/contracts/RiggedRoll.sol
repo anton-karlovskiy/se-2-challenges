e2c3944bc336d32a8da59d21e2ad68dde68477be
anton-karlovskiy <antonkarlovskiy@outlook.com>
2024-08-24 23:42:44 +0200

    chore: clean up

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -14,22 +14,24 @@ contract RiggedRoll is Ownable {
 
 
     // Implement the `withdraw` function to transfer Ether from the rigged contract to a specified address.
-    function withdraw(address _addr, uint256 _amount) public onlyOwner {
-        (bool succeed,) = payable(_addr).call{value : _amount}("");
-        require(succeed, "Failed to withdraw ether");
+    function withdraw(address to_, uint256 amount_) public onlyOwner {
+        (bool succeed,) = payable(to_).call{value: amount_}("");
+        require(succeed, "Failed to withdraw ether.");
     }
 
     // Create the `riggedRoll()` function to predict the randomness in the DiceGame contract and only initiate a roll when it guarantees a win.
     function riggedRoll() public {
         require(address(this).balance >= 0.002 ether, "Failed to have 0.002 ether in balance.");
+
         bytes32 prevHash = blockhash(block.number - 1);
-        uint256 _nonce = diceGame.nonce();
-        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), _nonce));
+        uint256 nonce = diceGame.nonce();
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), nonce));
         uint256 roll = uint256(hash) % 16;
 
-        console.log("THE ROLL IS ", roll);
-        require(roll <= 2, "THE ROLL IS a LOSER, bailing.");
-        console.log("THE ROLL IS a WINNER!");
+        console.log("The roll is", roll);
+        require(roll <= 5, "The roll is a loser so bailing.");
+        console.log("The roll is a winner!");
+        
         diceGame.rollTheDice{value: 0.002 ether}();
     }
 
